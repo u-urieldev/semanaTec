d627e595148927ce58e1da834306d16f2dea55c4
Uriel Aguilar <uaguilardeveloper@gmail.com>
2022-03-24 12:01:29 -0600

    Se cambia la velocidad de la tortuga

--- snake.py
+++ snake.py
@@ -52,7 +52,7 @@ def move():
 
     square(food.x, food.y, 9, 'green')
     update()
-    ontimer(move, 100)
+    ontimer(move, 150) #Se cambia la velocidad de la sepiente con el segundo parametro
 
 setup(420, 420, 370, 0)
 hideturtle()
