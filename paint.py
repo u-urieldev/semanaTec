b0f390c320ff939346caac8848726cd6c2a11bcd
Uriel Aguilar <78172208+u-urieldev@users.noreply.github.com>
2022-03-26 20:09:33 -0600

    Se añaden nombres

--- paint.py
+++ paint.py
@@ -1,12 +1,10 @@
-"""Paint, for drawing shapes.
+"""
+Paint, for drawing shapes.
 
-Exercises
+Uriel Aguilar
+Luca Boschetti
+Alejandro Arouesty
 
-1. Add a color.
-2. Complete circle.
-3. Complete rectangle.
-4. Complete triangle.
-5. Add width parameter.
 """
 
 from turtle import *
